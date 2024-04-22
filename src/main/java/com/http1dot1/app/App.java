cf1da602f59eac9f5f62ca2a46f14edbb1307aac
diegolikescode <diegoprestes.git@hotmail.com>
2024-04-22 17:19:25 -0300

    start the thing

---
+++ src/main/java/com/http1dot1/app/App.java
@@ -0,0 +1,13 @@
+package com.http1dot1.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/http1dot1/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.http1dot1.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
