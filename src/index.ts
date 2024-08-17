601e97de074adeaee3a6ff0e43aaacb9e7686026
diegolikescode <diegoprestes.git@hotmail.com>
2024-08-17 12:40:54 -0300

    legacy: ts version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("STARTS");
+import { createServer } from "./server.js";
+
+function startServer() {
+    const port = 3000;
+
+    const server = createServer();
+    server.listen(port, () => console.log(`server listening on port ${port}`));
+}
+
+startServer();
